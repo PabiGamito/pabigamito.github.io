8c9f9ac351ae92db0d062449dfd755f595e54914
Pablo Gamito <pablogamito@gmail.com>
2016-09-29 21:37:33 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Fun message
+console.log("Hello fellow developer,\n" +
+  "\nI see you are interesting in the source code of my personal website.\n" +
+  "If you have a job for me don't hesitate to contact me!\n" +
+  "\nWant to see more of my cool code? Check me out on GitHub: github.com/pabigamito\n"+
+  "Enjoy the code :)"
+);
