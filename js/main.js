2cb81f255b2c7425f2a047fc63f990c9ea6327c5
Pablo Gamito <pablogamito@gmail.com>
2016-09-30 23:01:56 +0200

    Added sections to portfolio website

--- js/main.js
+++ js/main.js
@@ -6,6 +6,12 @@ console.log("Hello fellow developer,\n" +
   "Enjoy the code :)"
 );
 
+// On reload load page from top
+$(window).on('beforeunload', function(){
+  $(window).scrollTop(0);
+});
+
+// TYPE-WRITER TEXT ANIMATION //
 var TxtRotate = function(el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
@@ -57,3 +63,81 @@ window.onload = function() {
     }
   }
 };
+
+// SCROLL TO NEXT SECTION ON MOUSE WHEEL //
+// TODO: Only make this run on desktop not on mobile
+//Set each section's height equals to the window height
+// $('section').height($(window).height());
+/*set the class 'active' to the first element
+ this will serve as our indicator*/
+$('section').first().addClass('active');
+// defines scrolling variable to false, when true doesn't do anything on mouse wheel to prevent overload
+var scrolling = false;
+
+/* handle the mousewheel event together with
+ DOMMouseScroll to work on cross browser */
+$(document).on('mousewheel DOMMouseScroll', function (e) {
+  e.preventDefault(); //prevent the default mousewheel scrolling
+  if (!scrolling) {
+    scrolling = true;
+    var active = $('section.active');
+    //get the delta to determine the mousewheel scrol UP and DOWN
+    var delta = e.originalEvent.detail < 0 || e.originalEvent.wheelDelta > 0 ? 1 : -1;
+
+    //if the delta value is negative, the user is scrolling down
+    if (delta < 0) {
+      //mousewheel down handler
+      next = active.next();
+      //check if the next section exist and animate the anchoring
+      if (next.length) {
+       /*setTimeout is here to prevent the scrolling animation
+        to jump to the topmost or bottom when
+        the user scrolled very fast.*/
+        var timer = setTimeout(function () {
+          /* animate the scrollTop by passing
+          the elements offset top value */
+          $('body, html').animate({
+            scrollTop: next.offset().top
+          }, 500, function(){
+            // Animation complete
+            scrolling = false;
+          });
+
+          // move the indicator 'active' class
+          next.addClass('active')
+            .siblings().removeClass('active');
+
+          clearTimeout(timer);
+        }, 200);
+      } else {
+        scrolling = false;
+      }
+
+    } else {
+      //mousewheel up handler
+      /*similar logic to the mousewheel down handler
+      except that we are animate the anchoring
+      to the previous sibling element*/
+      prev = active.prev();
+
+      if (prev.length) {
+        var timer = setTimeout(function () {
+          $('body, html').animate({
+            scrollTop: prev.offset().top
+          }, 500, function(){
+            // Animation complete
+            scrolling = false;
+          });
+
+          prev.addClass('active')
+              .siblings().removeClass('active');
+
+          clearTimeout(timer);
+        }, 200);
+      } else {
+        scrolling = false;
+      }
+
+    }
+  }
+});
